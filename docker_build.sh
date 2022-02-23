9d35beba8b7aa108eb9bf5622102b066263c1d37
Jackson <15jgme@queensu.ca>
2022-02-23 09:30:50 -0500

    Cache the uav_ros repo and change default to focal

--- docker_build.sh
+++ docker_build.sh
@@ -2,7 +2,7 @@
 
 echo "Building icuas22_competition Docker image"
 
-distro="bionic"
+distro="focal"
 build_args=""
 for (( i=1; i<=$#; i++));
 do
@@ -26,7 +26,19 @@ done
 
 echo "Building for $distro with additional docker arguments $build_args."
 
-docker build \
+
+# Check if UAV_ros repo exists
+
+if [[ "$(docker images -q lmark/uav_ros_simulation:focal 2> /dev/null)" == "" ]]; then
+  docker build \
+    $build_args \
+    --cache-from lmark1/uav_ros_simulation\
+    -f Dockerfile.$distro \
+    -t icuas22_competition:$distro .
+else
+  docker build \
     $build_args \
     -f Dockerfile.$distro \
     -t icuas22_competition:$distro .
+fi
+
