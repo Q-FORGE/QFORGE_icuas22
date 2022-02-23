d96afef927ed43c1673235064a909f45caf09d05
Jackson <15jgme@queensu.ca>
2022-02-23 16:46:19 -0500

    camera work

--- docker_build.sh
+++ docker_build.sh
@@ -29,7 +29,17 @@ echo "Building for $distro with additional docker arguments $build_args."
 
 # Check if UAV_ros repo exists
 
-if [[ "$(docker images -q lmark/uav_ros_simulation:focal 2> /dev/null)" == "" ]]; then
+if [ "$(docker images -q lmark/uav_ros_simulation:focal 2> /dev/null)" == "" -a "$(docker images -q icuas22_competition:focal 2> /dev/null)" == "" ];
+echo "UAV ROS and icuas_2022 images detected"
+then
+  docker build \
+    $build_args \
+    --cache-from lmark1/uav_ros_simulation\
+    --cache-from icuas22_competition\
+    -f Dockerfile.$distro \
+    -t icuas22_competition:$distro .
+elif [[ "$(docker images -q lmark/uav_ros_simulation:focal 2> /dev/null)" == "" ]]; then
+echo "UAV ROS image detected"
   docker build \
     $build_args \
     --cache-from lmark1/uav_ros_simulation\
